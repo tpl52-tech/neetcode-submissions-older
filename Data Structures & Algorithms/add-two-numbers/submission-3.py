ce2228bbef36e239c4f52f11081942ffa688db40
Tess (Hyunsuh) Lee <tpl52@cornell.edu>
2026-09-03 13:00:49 -0400

    Add: add-two-numbers - submission-3

---
+++ Data Structures & Algorithms/add-two-numbers/submission-3.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        carry = 0
+        cur = None 
+        
+        while l1 or l2 or (carry != 0): 
+            if l1 is not None: 
+                l1_val_raw = l1.val 
+                l1 = l1.next 
+            else: 
+                l1_val_raw = 0
+            if l2 is not None: 
+                l2_val_raw = l2.val 
+                l2 = l2.next 
+            else: 
+                l2_val_raw = 0
+
+            total_val_raw = l1_val_raw + l2_val_raw + carry
+            carry = total_val_raw // 10
+            total_val = total_val_raw % 10 
+
+            # handle the nodes. how to get the first one? 
+            if cur is None: 
+                head = ListNode(total_val)
+                cur = head
+            else: 
+                new = ListNode(total_val)
+                cur.next = new 
+                cur = cur.next 
+            
+        
+        return head 
+            
+
+
+
+    
