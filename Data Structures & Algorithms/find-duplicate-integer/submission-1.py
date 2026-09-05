5243051369eee5cae1caaac5003e8a7890a32982
Tess (Hyunsuh) Lee <tpl52@cornell.edu>
2026-09-05 16:01:14 -0400

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # I DID HAVE CYCLE INTUITION I HAD THE FUCKIN CYCLE INTUITION
+
+        # iterate through the list, go to the index of the value written 
+        # (since the range is [1,n]). it's going to go to
+        # the same index, then it's going to make a loop back and 
+        # fourth between the two numbers. 
+
+        current = -1 
+
+        for i in range (len(nums)): 
+
+            prev = current 
+
+            if current == -1 :
+                current = nums[i]
+            else: 
+                current = nums[nums[i]]
+                if prev == nums[current] and current == nums[prev]: 
+                    return nums[prev]
+            
+        return -1
+
+
+
+
+        
+
